5bcc83a112293bc98831613345aaec946b5931cc
Eduardo Costa <eduardo.costa.ribeiro@live.com>
2013-11-28 22:22:31 -0200

    inclusao de JARs e classe de conexao

---
+++ TrabalhoBiblioteca/src/br/com/lacerda/conexao/ConexaoOracle.java
@@ -0,0 +1,56 @@
+
+package br.com.lacerda.conexao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import oracle.jdbc.pool.OracleDataSource;
+
+
+/**
+ *
+ * @author Stwuart
+ */
+
+public class ConexaoOracle {
+    private boolean conected;
+    private UsuarioOracle usuario;
+    private UsaXML xml;
+    Connection con;
+
+    public Connection getConnection() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, Exception {
+	this.xml = new UsaXML();
+        this.conected = false;
+	this.usuario = this.xml.leituraXML();
+                
+        try{
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL(this.usuario.getUrl());
+            ods.setPassword(this.usuario.getSenha());
+            ods.setUser(this.usuario.getUsuario());
+            this.con = ods.getConnection();
+            this.con.setAutoCommit(true);
+            this.conected = true;
+
+            return this.con;
+        } catch (SQLException ex) {
+            throw new Exception ("Erro ConexaoOracle.getConnection\n"+ ex.getMessage());
+        }
+	
+	
+}
+
+    public void disconecta() throws SQLException, Exception{
+        try{
+            if(checkConection()){
+                this.conected = false;                
+                this.con.close();
+            }
+        }catch(SQLException ex) {
+            throw new Exception ("Erro ConexaoOracle.disconecta\n"+ ex.getMessage());
+        }
+    }
+
+    public boolean checkConection(){
+            return this.conected;
+    }
+}
