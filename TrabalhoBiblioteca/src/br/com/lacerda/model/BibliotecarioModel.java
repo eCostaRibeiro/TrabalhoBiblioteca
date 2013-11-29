4d6a54551377b6e8b7a8a2ccdb88b975c9cdbcbb
rafael <rafaeltakiyama@gmail.com>
2013-11-29 21:08:10 -0200

    model pronto

---
+++ TrabalhoBiblioteca/src/br/com/lacerda/model/BibliotecarioModel.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lacerda.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Rafael
+ */
+public class BibliotecarioModel extends PessoaModel implements Serializable {
+
+    private String data_admissao;
+    private String CRB;
+    private int salario;
+
+    /**
+     * @return the data_admissao
+     */
+    public String getData_admissao() {
+        return data_admissao;
+    }
+
+    /**
+     * @param data_admissao the data_admissao to set
+     */
+    public void setData_admissao(String data_admissao) {
+        this.data_admissao = data_admissao;
+    }
+
+    /**
+     * @return the CRB
+     */
+    public String getCRB() {
+        return CRB;
+    }
+
+    /**
+     * @param CRB the CRB to set
+     */
+    public void setCRB(String CRB) {
+        this.CRB = CRB;
+    }
+
+    /**
+     * @return the salario
+     */
+    public int getSalario() {
+        return salario;
+    }
+
+    /**
+     * @param salario the salario to set
+     */
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+}
